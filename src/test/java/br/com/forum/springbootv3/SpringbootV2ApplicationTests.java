cd5159cb667c2dfff2524eccec0b46facb2468f0
felipeam10 <felipeam10@hotmail.com>
2022-01-18 01:27:05 -0300

    v3.1 inicializando os testes automatizados com spring boot

--- src/test/java/br/com/forum/springbootv3/SpringbootV2ApplicationTests.java
+++ src/test/java/br/com/forum/springbootv3/SpringbootV2ApplicationTests.java
@@ -1,13 +1,18 @@
 package br.com.forum.springbootv3;
 
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class SpringbootV2ApplicationTests {
 
 	@Test
 	void contextLoads() {
+		Assert.assertTrue(true);
 	}
 
 }
